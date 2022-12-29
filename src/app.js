ef9f1ea0598278ea4c9321ca858828cfc3b3b95c
Pétala Leite <petalaleite@gmail.com>
2022-12-29 15:57:59 -0300

    Updating missions.json

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const { readMissionsData } = require('./utils/fsUtils')
 
 const app = express()
+
+app.get('/missions', async (req, res) => {
+  const missions = await readMissionsData()
+  return res.status(200).json( { missions })
+})
 
 module.exports = app
