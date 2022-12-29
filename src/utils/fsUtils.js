5944f7d2c6046f484b9adb83600e93445fca93f8
Pétala Leite <petalaleite@gmail.com>
2022-12-29 14:09:07 -0300

    Adding files

---
+++ src/utils/fsUtils.js
@@ -0,0 +1,32 @@
+const fs = require('fs').promises;
+const path = require('path')
+
+const MISSION_DATA_PATH = '../../data/missions.json'
+
+async function readMissionsData() {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH))
+    const missions = JSON.parse(data);
+    console.log(missions)
+    return missions;
+  } catch(error) {
+    console.error(`Erro na leitura do arquivo: ${error}`)
+  }
+}
+
+async function writeNewMissionData(newMission) {
+  try {
+    const oldMissions = await readMissionsData();
+    const allMissions = JSON.stringify([
+      ...oldMissions, 
+      { id: Date.now(), ...newMission}])
+
+    await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMissions)
+  } catch(error) {
+    console.error(`Erro na escrita do arquivo: ${error}`)
+  }
+}
+module.exports = {
+  readMissionsData,
+  writeNewMissionData,
+}
